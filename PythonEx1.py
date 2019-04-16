a72028842be4d141463fbd7fb4b732ff48931c29
Michael Dao <michaeltuandao@gmail.com>
2019-04-16 15:10:22 -0500

    updated exercises

--- PythonEx1.py
+++ PythonEx1.py
@@ -1,1 +1,14 @@
-print('Hello World')
+#1
+name = input('What is your name?')
+print("Hello {}".format(name))
+
+#2
+name = input('What is your name?')
+letters = 0
+while letters < len(name):
+    letters += 1
+
+print('HELLO, {}!'.format(name.upper()))
+print('YOUR NAME HAS {} LETTERS IN IT! AWESOME!'.format(letters))
+
+#3
