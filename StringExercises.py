38f4660265502428ee3f4ccbf66db2dbb5e12fd4
Michael Dao <michaeltuandao@gmail.com>
2019-04-18 00:58:09 -0500

    having trouble with 4

---
+++ StringExercises.py
@@ -0,0 +1,26 @@
+#1
+word = "askjdhlask"
+print(word.upper())
+
+#2
+word = "alksjdhla"
+print(word.capitalize())
+
+#3
+word = "kajsdhla"
+new = ''
+for i in word:
+    new = i + new
+print(new)
+
+#4
+leet = [['A','4'], ['E','3'], ['G','5'], ['I', '1'], ['O', '0'], ['S', '5'], ['T', '7']]
+word = 'leet'
+word = word.upper()
+new_word = ''
+for i in word:
+    if i == (leet[][]]):
+        new_word = i.replace([][])
+    else:
+        new_word += i
+print(word)
