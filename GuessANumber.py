d1d38fdf3e0d13a4cd02b3e921f5ae251e3f1a6e
Michael Dao <michaeltuandao@gmail.com>
2019-04-17 23:58:20 -0500

    now prints a message if player wins

--- GuessANumber.py
+++ GuessANumber.py
@@ -2,16 +2,19 @@ import random
 
 my_random_number = random.randint(1,10)
 answer = int(input("I am thinking of a number between 1 and 10.\n what's the number?" ))
+print(my_random_number)
 
 guesses = 4
 while answer != my_random_number and guesses > 0:
-    if answer >= my_random_number:
+    if answer > my_random_number and guesses > 0:
         answer = int(input("{} is too high. You have {} guesses left. What's the number? ".format(answer, guesses)))
         guesses -= 1
-    elif answer < my_random_number:
+    elif answer < my_random_number and guesses > 0:
         answer = int(input("{} is too low. You have {} guesses left. What's the number? ".format(answer, guesses)))
         guesses -= 1
-    elif answer == my_random_number:
-        print("Yes! You win!")
 else:
-    print("You ran out of guesses. You lose!")
+    print("You ran out of guesses!")
+
+if answer == my_random_number:
+    print("Yes! You win!")
+
