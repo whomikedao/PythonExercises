a22d30c748c467d2be78b92cc72f773154ea5897
Michael Dao <michaeltuandao@gmail.com>
2019-04-22 09:26:04 -0500

    Finished functions exercise

---
+++ FunctionEx.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plot
+import math
+from numpy import arange
+
+#1
+# jaja = input("What's your name? ")
+
+# def hello(name):
+#     return("hello {}".format(name))
+
+# print(hello(jaja))
+
+#2
+# def f(x):
+#     return x + 1
+
+# xs = list(range(-3, 4))
+# ys = []
+
+# for x in xs:
+#     ys.append(f(x))
+
+# plot.plot(xs, ys)
+# plot.show()
+
+#3
+# def square(x):
+#     return x**2
+
+# xs = list(range(-100, 101))
+# ys = []
+
+# for x in xs:
+#     ys.append(square(x))
+
+# plot.plot(xs, ys)
+# plot.show
+
+#4
+# def odd_or_even(x):
+#     for numbers in range(-x, x+1):
+#         if(numbers + 1)%2 == 0:
+#             return 1
+#         elif numbers % 2 == 0:
+#             return -1
+
+# xs = list(range(-5, 6))
+# ys = []
+
+# for x in xs:
+#     ys.append(odd_or_even(x))
+
+# plot.bar(xs, ys)
+# plot.show
+
+#5
+# def sine(x):
+#     return sin(x)
+
+# xs = list(range(-5, 6))
+# ys = []
+
+# for x in xs:
+#     ys.append(sine(x))
+
+# plot.plot(xs, ys)
+# plot.show
+
+#6
