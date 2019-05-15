3a7a81e920518d12e1dcfe089976dd08597d163e
Michael Dao <michaeltuandao@gmail.com>
2019-05-15 09:55:27 -0500

    updated files

---
+++ class.py
@@ -0,0 +1,42 @@
+# name = 'Michael'
+# age = 5
+# sentence = '%s is %d years old' % (name, age)
+# print(sentence)
+
+
+# first_name = 'Michael'
+# last_name = 'Dao'
+
+# sentence = 'Hello {} {}'.format(first_name, last_name)
+# sentence_one = 'Hello ' + first_name + ' ' + last_name
+# sentence_two = 'Hello {0}. I believe your last name is {1} right, {0}?'.format(first_name, last_name)
+
+# print(sentence)
+# print(sentence_one)
+# print(sentence_two)
+
+# name = input('What is your name?')
+# sentence = 'The users name is {}'.format(name)
+# print(sentence)
+# age = 2019 - int(input('What is your year of birth?'))
+# sentence_age = 'The users age is {}'.format(age)
+# print(sentence_age)
+
+# if age >= 21:
+#     print("You get free beer")
+# else:
+#     print('Youre not allowed alcohol')
+
+# people = 20
+# cats = 30
+# dogs = 15
+
+# if(people < cats):
+#     print("Too many cats! the world is doomed!")
+# if(people > cats):
+#     print("Not many cats! The world is saved!")
+# if(people < dogs):
+#     print("The world is drooled on!")
+# if(people > dogs):
+#     print("The world is dry!")
+
